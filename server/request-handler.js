9a9f40ee04e6ff4f03960a0c7a3ce52ff4ea9f16
GMatias93 <gabriel.2m.deleon@gmail.com>
2015-10-27 19:29:53 -0700

    Got a message rendered on the chatterbox!

--- server/request-handler.js
+++ server/request-handler.js
@@ -15,10 +15,7 @@ exports.requestHandler = function(request, response) {
     // See the note below about CORS headers.
     var headers = exports.defaultCorsHeaders;
 
-
     headers['contentType'] = "application/json";
-
-
 
     //created an object 
     var data = {
@@ -26,9 +23,10 @@ exports.requestHandler = function(request, response) {
         results : [],
     };
 
+//if an Object ID is actually just the object's location in an array, then what can that tell us about how to supply it to the client?
 
-
-    var objectId = {username: 'Jono', message: 'Do my bidding!'};
+    var message = {username: 'Jono', message: 'Do my bidding!'};
+    message.objectId = 0;
     // same as results[0]
 
     console.log('request.url is:',request.url);
@@ -40,20 +38,21 @@ exports.requestHandler = function(request, response) {
         }        
         response.writeHead(statusCode, headers);        
         // response.write(JSON.stringify(data));
-        response.end(JSON.stringify({results : [objectId]}));
+        response.end(JSON.stringify({results : [message]}));
 
     } else if (request.method === 'POST'){
         // console.log('request.json i:', request);
         //push post data into end:
         data.results.push(request.json);
+        ++objectId;
         //modify data as requested
         //update the stored value of the data
         response.writeHead(201, headers);
-        response.end(JSON.stringify(data));
+        response.end(JSON.stringify({results : [message]}));
     } else if (request.method === 'OPTIONS') {
         response.writeHead(statusCode, headers);
     }
-    
+
   };
 
 
